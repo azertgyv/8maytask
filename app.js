c5558dbc8dbe2cb68075f02f2fbf9e350ec90c9a
azertgyv <atagiyev077@gmail.com>
2023-05-11 10:26:52 +0400

    8maytask

---
+++ app.js
@@ -0,0 +1,56 @@
+// const people = [
+//     { name: 'Azer', age: 25, job: 'dev' }
+// ]
+// people[0].name='AZER'
+// console.log(people)
+
+// const arr = [];
+
+// for(let i = 0; i<3; i++){
+//     let num = prompt("3 eded girin")
+//     arr.push(Number(num))
+// }
+
+// let sum = 0;
+// for(let i =0; i <arr.length; i++){
+//     sum+= arr[i]
+// }
+
+// console.log(sum)
+
+// const arr2=[
+//     "zeyneb 1798",
+//     "eshgin 2000",
+//     "cavidan 1879",
+//     "ali 1978"
+// ]
+// arr2.sort((a,b)=>b.slice(-4)-a.slice(-4))
+// console.log(arr2)
+
+// const cm2 = ['ali', 'eshqin', 'ayla', 'zeyneb', 'aytac', 'tariyel', 'shehla']
+// const indexFinder = cm2.indexOf('tariyel')
+// if (indexFinder !== -1) {
+//     console.log('Not found')
+// }
+// console.log(indexFinder)
+
+// let arr = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+// function filterByWith(letter) {
+//   let result = [];
+//   for (let element of arr) {    if (element.includes(letter)) {    result.push(element);   }   }
+//   return result;
+// }
+// let filteredArr = filterByWith('t');
+// filteredArr.length !== 0 ?  console.log(filteredArr) : console.log("No elements")
+
+
+// let arr = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+//  const firstUpper  =  arr => {
+//   let newArr = []
+//   for (let element of arr) {
+//     newArr.push(element.charAt(0).toUpperCase() + element.slice(1))
+//   }
+//   return newArr
+// }  
+// console.log(firstUpper(arr))
+
